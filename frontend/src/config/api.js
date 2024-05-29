f7b70a41a70755677a068de776a1fcbe1e17cb82
Kamlesh Kumar <65109934+kamkode@users.noreply.github.com>
2024-05-29 05:54:20 +0530

    Update api.js

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = "http://localhost:5454";
+export const API_URL = "https://backend-food-re0f.onrender.com";
 
 
 export const api = axios.create({
